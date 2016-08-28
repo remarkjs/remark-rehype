04d48e1aece482e33dd614b35857a69bad3c61ed
Titus Wormer <tituswormer@gmail.com>
2016-08-28 11:23:08 +0200

    Update dev-dependencies

--- index.js
+++ index.js
@@ -8,50 +8,32 @@
 
 'use strict';
 
-/* eslint-env commonjs */
-
 /* Dependencies */
 var mdast2hast = require('mdast-util-to-hast');
 
-/**
- * Mutate-mode.  Further transformers run on the HAST tree.
- *
- * @param {MDASTNode} node - Tree.
- * @return {HASTNode} - Tree.
- */
-function mutate(node) {
-    return mdast2hast(node);
-}
+/* Expose. */
+module.exports = attacher;
 
-/**
- * Bridge-mode.  Runs the destination with the new HAST
- * tree.
- *
- * @param {Unified} destination - Destination processor.
- * @return {Function} - Transformer.
- */
-function bridge(destination) {
-    return function (node, file, next) {
-        destination.run(mdast2hast(node), file, function (err) {
-            next(err);
-        });
-    };
-}
-
-/**
- * Attacher.
+/* Attacher.
  * If a destination is given, runs the destination with
  * the new HAST tree (bridge-mode).
  * Without destination, returns the HAST tree: further
- * plug-ins run on that tree (mutate-mode).
- *
- * @param {Unified} origin - Origin processor.
- * @param {Unified} [destination] - Destination processor.
- * @return {Function} - Transformer.
- */
+ * plug-ins run on that tree (mutate-mode). */
 function attacher(origin, destination) {
-    return destination ? bridge(destination) : mutate;
+  return destination ? bridge(destination) : mutate;
 }
 
-/* Expose. */
-module.exports = attacher;
+/* Bridge-mode.  Runs the destination with the new HAST
+ * tree. */
+function bridge(destination) {
+  return function (node, file, next) {
+    destination.run(mdast2hast(node), file, function (err) {
+      next(err);
+    });
+  };
+}
+
+/* Mutate-mode.  Further transformers run on the HAST tree. */
+function mutate(node) {
+  return mdast2hast(node);
+}
--- test.js
+++ test.js
@@ -8,41 +8,37 @@
 
 'use strict';
 
-/* eslint-env node */
-/* jscs:disable jsDoc */
-/* jscs:disable maximumLineLength */
-
 /* Dependencies. */
 var test = require('tape');
 var unified = require('unified');
 var parse = require('remark-parse');
-var remark2rehype = require('./index.js');
 var markdown = require('remark-stringify');
 var html = require('rehype-stringify');
+var remark2rehype = require('./index.js');
 
 /* Tests. */
 test('remark2rehype()', function (t) {
-    t.equal(
-        unified()
-            .use(parse)
-            .use(remark2rehype)
-            .use(html)
-            .process('## Hello, world! ##')
-            .toString(),
-        '<h2>Hello, world!</h2>',
-        'should mutate'
-    );
+  t.equal(
+    unified()
+      .use(parse)
+      .use(remark2rehype)
+      .use(html)
+      .process('## Hello, world! ##')
+      .toString(),
+    '<h2>Hello, world!</h2>',
+    'should mutate'
+  );
 
-    t.equal(
-        unified()
-            .use(parse)
-            .use(remark2rehype, unified())
-            .use(markdown)
-            .process('## Hello, world! ##')
-            .toString(),
-        '## Hello, world!\n',
-        'should bridge'
-    );
+  t.equal(
+    unified()
+      .use(parse)
+      .use(remark2rehype, unified())
+      .use(markdown)
+      .process('## Hello, world! ##')
+      .toString(),
+    '## Hello, world!\n',
+    'should bridge'
+  );
 
-    t.end();
+  t.end();
 });
