e211ff77febfcde868291e2f34fc1fa426c727a1
Takagi Kensuke <janus.wel.3@gmail.com>
2021-04-21 15:42:21 +0900

    :bug: fix type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,14 +2,11 @@
 import {Plugin, Processor} from 'unified'
 import {Options} from 'mdast-util-to-hast'
 
-type Settings =
-  | [Options?]
-  | {
-      length: number
-      0?: Processor
-      1?: Options
-    }
+interface Settings extends Array<any> {
+  0?: Processor
+  1?: Options
+}
 
-declare const remark2rehype: Plugin<Settings>
+declare const remark2rehype: Plugin<[Options?] | Settings>
 
 export = remark2rehype
