d5c9582955572846c091bb1b077c8c2d943a6006
Titus Wormer <tituswormer@gmail.com>
2021-12-03 11:46:50 +0100

    Update dev-dependencies

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
  * @typedef {import('./lib/index.js').Processor} Processor
  */
 
-import remarkRehype from './lib/index.js'
-
 export {defaultHandlers, all, one} from 'mdast-util-to-hast'
 
-export default remarkRehype
+export {default} from './lib/index.js'
