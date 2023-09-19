102e10e4af2581955ec6d999bda358fda803acc8
Titus Wormer <tituswormer@gmail.com>
2023-09-19 14:59:35 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,73 +1,81 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {unified} from 'unified'
 import remarkParse from 'remark-parse'
 import remarkMarkdown from 'remark-stringify'
 import rehypeStringify from 'rehype-stringify'
-import remarkRehype, {defaultHandlers, all, one} from './index.js'
+import remarkRehype from './index.js'
 
-test('exports', (t) => {
-  t.assert(defaultHandlers, 'should export `defaultHandlers`')
-  t.assert(one, 'should export `one`')
-  t.assert(all, 'should export `all`')
+test('remarkRehype', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'all',
+      'default',
+      'defaultHandlers',
+      'one'
+    ])
+  })
 
-  t.end()
-})
+  await t.test('should mutate', async function () {
+    assert.equal(
+      unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeStringify)
+        .processSync('## Hello, world! ##')
+        .toString(),
+      '<h2>Hello, world!</h2>'
+    )
+  })
 
-test('remarkRehype', (t) => {
-  t.equal(
-    unified()
-      .use(remarkParse)
-      .use(remarkRehype)
-      .use(rehypeStringify)
-      .processSync('## Hello, world! ##')
-      .toString(),
-    '<h2>Hello, world!</h2>',
-    'should mutate'
+  await t.test('should mutate with options', async function () {
+    assert.equal(
+      unified()
+        .use(remarkParse)
+        .use(remarkRehype, {allowDangerousHtml: true})
+        .use(rehypeStringify, {allowDangerousHtml: true})
+        .processSync('## Hello, <i>world</i>! ##')
+        .toString(),
+      '<h2>Hello, <i>world</i>!</h2>'
+    )
+  })
+
+  await t.test(
+    'should mutate with `processor: null` and options',
+    async function () {
+      assert.equal(
+        unified()
+          .use(remarkParse)
+          .use(remarkRehype, null, {allowDangerousHtml: true})
+          .use(rehypeStringify, {allowDangerousHtml: true})
+          .processSync('## Hello, <i>world</i>! ##')
+          .toString(),
+        '<h2>Hello, <i>world</i>!</h2>'
+      )
+    }
   )
 
-  t.equal(
-    unified()
-      .use(remarkParse)
-      .use(remarkRehype, {allowDangerousHtml: true})
-      .use(rehypeStringify, {allowDangerousHtml: true})
-      .processSync('## Hello, <i>world</i>! ##')
-      .toString(),
-    '<h2>Hello, <i>world</i>!</h2>',
-    'should mutate with options'
-  )
+  await t.test('should bridge', async function () {
+    assert.equal(
+      unified()
+        .use(remarkParse)
+        .use(remarkRehype, unified())
+        .use(remarkMarkdown)
+        .processSync('## Hello, world! ##')
+        .toString(),
+      '## Hello, world!\n'
+    )
+  })
 
-  t.equal(
-    unified()
-      .use(remarkParse)
-      .use(remarkRehype, null, {allowDangerousHtml: true})
-      .use(rehypeStringify, {allowDangerousHtml: true})
-      .processSync('## Hello, <i>world</i>! ##')
-      .toString(),
-    '<h2>Hello, <i>world</i>!</h2>',
-    'should mutate with `processor: null` and options'
-  )
-
-  t.equal(
-    unified()
-      .use(remarkParse)
-      .use(remarkRehype, unified())
-      .use(remarkMarkdown)
-      .processSync('## Hello, world! ##')
-      .toString(),
-    '## Hello, world!\n',
-    'should bridge'
-  )
-
-  t.equal(
-    unified()
-      .use(remarkParse)
-      .use(remarkRehype, unified(), {allowDangerousHtml: true})
-      .use(remarkMarkdown)
-      .processSync('## Hello, <i>world</i>! ##')
-      .toString(),
-    '## Hello, <i>world</i>!\n',
-    'should bridge with options'
-  )
-
-  t.end()
+  await t.test('should bridge with options', async function () {
+    assert.equal(
+      unified()
+        .use(remarkParse)
+        .use(remarkRehype, unified(), {allowDangerousHtml: true})
+        .use(remarkMarkdown)
+        .processSync('## Hello, <i>world</i>! ##')
+        .toString(),
+      '## Hello, <i>world</i>!\n'
+    )
+  })
 })
